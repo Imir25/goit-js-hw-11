6e2d366baf5df6de18759f532ba4e8fd7f2be2a3
Imir25 <anitra26121998@gmail.com>
2023-10-04 22:32:47 +0300

    fixed error gallery

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const loadMoreButton = document.querySelector('.load-more');
 const searchForm = document.getElementById('search-form');
@@ -8,6 +10,7 @@ const resultsContainer = document.querySelector('.results-container');
 
 let page = 1;
 let totalHits = 0;
+const lightbox = new SimpleLightbox('.results-container a');
 
 searchForm.addEventListener('submit', async (e) => {
   e.preventDefault();
@@ -35,6 +38,7 @@ searchForm.addEventListener('submit', async (e) => {
       Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
       loadMoreButton.style.display = 'none';
     } else {
+      totalHits = data.totalHits;
       displaySearchResults(data.hits);
     }
   } catch (error) {
@@ -49,11 +53,21 @@ function displaySearchResults(results) {
     return;
   }
   results.forEach((result) => {
+    const imageLink = document.createElement('a');
+    imageLink.href = result.largeImageURL;
+    imageLink.classList.add('gallery-item');
+
     const imageElement = document.createElement('img');
     imageElement.src = result.webformatURL;
     imageElement.alt = result.tags;
-    resultsContainer.appendChild(imageElement);
+    imageLink.appendChild(imageElement);
+
+    resultsContainer.appendChild(imageLink);
   });
+
+  lightbox.refresh();
+
+  Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
 }
 
 const showEndOfResultsMessage = () => {
@@ -62,85 +76,7 @@ const showEndOfResultsMessage = () => {
 
 loadMoreButton.addEventListener('click', async () => {
   page += 1;
-  
-  try {
-    const response = await axios.get('https://pixabay.com/api/', {
-      params: {
-        key: '39801546-c4bb34864e6abc7825d1e4868',
-        q: searchInput.value.trim(),
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true,
-        page,
-        per_page: 40,
-      },
-    });
 
-    const data = response.data;
-    if (data.hits.length === 0) {
-      showEndOfResultsMessage();
-      loadMoreButton.style.display = 'none';
-    } else {
-      displaySearchResults(data.hits);
-      totalHits = data.totalHits;
-      if (page * 40 >= totalHits) {
-        showEndOfResultsMessage();
-        loadMoreButton.style.display = 'none';
-      } else {
-        loadMoreButton.style.display = 'block';
-      }
-    }
-  } catch (error) {
-    console.error('Помилка запиту:', error);
-    Notiflix.Notify.failure('Oops! Something went wrong. Please try reloading the page.');
-  }
-});
-
-const searchImages = async () => {
-  try {
-    const response = await axios.get('https://pixabay.com/api/', {
-      params: {
-        key: '39801546-c4bb34864e6abc7825d1e4868',
-        q: searchInput.value.trim(),
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true,
-        page,
-        per_page: 40,
-      },
-    });
-
-    const data = response.data;
-    if (data.hits.length === 0) {
-      showEndOfResultsMessage();
-      loadMoreButton.style.display = 'none';
-    } else {
-      displaySearchResults(data.hits);
-      totalHits = data.totalHits;
-      if (page * 40 >= totalHits) {
-        showEndOfResultsMessage();
-        loadMoreButton.style.display = 'none';
-      } else {
-        loadMoreButton.style.display = 'block';
-      }
-    }
-  } catch (error) {
-    console.error('Помилка запиту:', error);
-    Notiflix.Notify.failure('Oops! Something went wrong. Please try reloading the page.');
-  }
-};
-
-searchForm.addEventListener('submit', async (e) => {
-  e.preventDefault();
-  page = 1;
-  totalHits = 0;
-  loadMoreButton.style.display = 'none';
-  searchImages();
-});
-
-loadMoreButton.addEventListener('click', async () => {
-  page += 1;
-  
   try {
     const response = await axios.get('https://pixabay.com/api/', {
       params: {
@@ -175,5 +111,50 @@ loadMoreButton.addEventListener('click', async () => {
 });
 
 window.addEventListener('load', () => {
+  resultsContainer.style.display = 'none';
+});
+
+searchForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  page = 1;
+  totalHits = 0;
+  loadMoreButton.style.display = 'none';
+  resultsContainer.style.display = 'none';
   searchImages();
 });
+
+const searchImages = async () => {
+  try {
+    const response = await axios.get('https://pixabay.com/api/', {
+      params: {
+        key: '39801546-c4bb34864e6abc7825d1e4868',
+        q: searchInput.value.trim(),
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page,
+        per_page: 40,
+      },
+    });
+
+    const data = response.data;
+    if (data.hits.length === 0) {
+      showEndOfResultsMessage();
+      loadMoreButton.style.display = 'none';
+    } else {
+      displaySearchResults(data.hits);
+      totalHits = data.totalHits;
+      if (page * 40 >= totalHits) {
+        showEndOfResultsMessage();
+        loadMoreButton.style.display = 'none';
+      } else {
+        loadMoreButton.style.display = 'block';
+      }
+    }
+
+    resultsContainer.style.display = 'block';
+  } catch (error) {
+    console.error('Помилка запиту:', error);
+    Notiflix.Notify.failure('Oops! Something went wrong. Please try reloading the page.');
+  }
+};
